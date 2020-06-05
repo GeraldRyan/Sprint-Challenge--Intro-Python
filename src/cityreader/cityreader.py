c59159545f8fd9ba005b6707509ceb11f8925c80
GeraldRyan <gerald.ryan40@yahoo.com>
2020-06-05 11:18:54 -0500

    now cities passes.... need to float the ints

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -5,8 +5,8 @@ import csv
 class City:
   def __init__(self, name, lat, lon):
     self.name = name
-    self.lat = lat
-    self.lon = lon
+    self.lat = float(lat)
+    self.lon = float(lon)
 
   def __str__(self):
     return f" Name: {self.name} Coordinates: {self.lat} {self.lon}"
@@ -34,7 +34,7 @@ def cityreader(cities=[]):
       csv_reader = csv.reader(csvfile)
       next(csv_reader)
       for line in csv_reader:
-        city = City(line[0], line[3], line[4])
+        city = City(line[0], float(line[3]), float(line[4]))
         cities.append(city)
     return cities
 
@@ -42,7 +42,7 @@ cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(f"CITY:{c}")
+    print(f"CITY: {c}")
 
 # STRETCH GOAL!
 #
